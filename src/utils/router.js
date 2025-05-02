8e59d83d325fdf42fe2350411be42abd325456ba
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-02 21:43:54 +0200

    router boilerplate

---
+++ src/utils/router.js
@@ -0,0 +1,18 @@
+const routes =
+{
+	'/': () => import('../pages/home.js'),
+	'/about': () => import('../pages/about.js'),
+	'/contact': () => import('../pages/home.js'),
+}
+
+export async function handleRoute() {
+	const path = window.location.pathname;
+	const route = routes[path] || routes['/'];
+	const module = await route();
+
+	const { render } = module;
+	render(document.querySelector('#app'));
+}
+
+window.addEventListener('popstate', handleRoute);
+handleRoute();
