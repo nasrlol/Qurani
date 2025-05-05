db91afa49b4ae4e38cb04cef32f8eef3b5481bac
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-05 22:24:11 +0200

    section update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,34 @@
 "use strict";
 
-import { nav } from "../components/nav.js";
-import { ayahOfTheDay } from "../components/ayaOfTheDay";
-import { footer } from "../components/footer.js";
+import {nav} from "../components/nav.js";
+import {ayahOfTheDay} from "../components/ayahDay.js";
+import {footer} from "../components/footer.js";
+import {surahOfTheDay} from "../components/surahDay.js";
 
 export const home = `
 
 	${nav}	
 	<main>
 	<header>
-		<h1>Welcome to the Quran App</h1>
-		<p>Your daily connection with the Quran</p>
+		<h1>ٱلسَّلَامُ عَلَيْكُمْ</h1>
 	</header>
 	<main>
-		<section id="ayah-of-the-day">
-			<h2>Ayah of the Day</h2>
-			${ayahOfTheDay}
-		</section>
+		<div class="featuresOfTheDay">
+			<section id="ayah-of-the-day">
+				<h2>Ayah of the Day</h2>
+				${ayahOfTheDay}
+			</section>
+			<section id="surah-of-the-day">
+				<h2>Surah of the day</h2>
+				${surahOfTheDay}
+			</section>
+		</div>
 
 		<section id="features">
 			<h2>Explore Features</h2>
 			<ul>
-				<li>Search by keywords</li>
-				<li>Study tafsir and translations</li>
+				<li><a>Search by keywords</a></li>
+				<li>tafsir and translations</li>
 			</ul>
 		</section>
 	</main>
