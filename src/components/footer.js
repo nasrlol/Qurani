79d95618eee1ebe13974fc3651f4cac099b61553
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-12 22:45:43 +0200

    removed the dark mode button because i want to add it to the navigation bar

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,6 @@
 export const footer = `
     
     <footer>
-        <button id="darkModeButton">Light</button>
         <p>&copy nasr</p>
         <p><a href="https://www.github.com/nasrlol">Github</a></p>
     </footer>
