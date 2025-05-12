017e8dab679660684761eccad89228854ef8a50f
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-12 22:58:27 +0200

    update 12/05

--- src/api/text/surah.js
+++ src/api/text/surah.js
@@ -19,10 +19,10 @@ export async function surahDay() {
 		if (!response.ok) throw new Error("failed to fetch the URL");
 
 		const data = await response.json();
-		// arrow function dat arrays.map gebruitk naar het schijnt, het fijne snap ik er nog niet van maar het zet het object
+		// arrow function dat arrays.map gebruitk naar het schijnt, het fijne snap ik er nog niet van, maar het zet het object
 		// om naar tekst en dat is handig voor de output, dan nog eens het nummer van de aya erbij en je hebt al een goeie representatie
 		// van hoe een surah in een quran er uit zou moeten zien
-		// TO DO: het uitklap baar maken zodat het wat beter handel baar is voor de UI
+		// TO DO: het uitklapbaar maken zodat het wat beter handelbaar is voor de UI
 		return data.data.ayahs.map((ayah) => `${ayah.text} ${ayah.number}`);
 	} catch (error) {
 		console.error("there was a problem fetching a random ayah: ", error);
