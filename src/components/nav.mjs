4445f1116211219b4d89ad8c9f5584d3711d5866
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-13 20:06:31 +0200

    [RESTRUCTURE] removed a couple of pages and removed their routes

--- src/components/nav.mjs
+++ src/components/nav.mjs
@@ -1,14 +1,15 @@
 "use strict";
 
 export const nav = `
+<header>
   <nav>
     <ul>
       <li><a href="/" data-link>Home</a></li> 
       <li><a href="/quran" data-link>Quran</a></li>
       <li><a href="/tafsir" data-link>Tafsir</a></li>
-      <li><a href="/translation" data-link>Translation</a></li>
       <li><a href="/about" data-link>About</a></li>
-      <li><a href="/contact" data-link>Contact</a></li>
     </ul>
   </nav>
+</header>
+
 `;
--- src/utils/router.js
+++ src/utils/router.js
@@ -1,26 +1,24 @@
 const routes = {
-	"/": () => import("../pages/home.js"),
-	"/quran": () => import("../pages/quran.js"),
-	"/tafsir": () => import("../pages/tafsir.js"),
-	"/translation": () => import("../pages/translation.js"),
-	"/about": () => import("../pages/about.js"),
-	"/contact": () => import("../pages/contact.js"),
+    "/": () => import("../pages/home.js"),
+    "/quran": () => import("../pages/quran.js"),
+    "/tafsir": () => import("../pages/tafsir.js"),
+    "/about": () => import("../pages/about.js"),
 };
 
 export async function handleRoute() {
-	const path = window.location.pathname;
-	const loadModule = routes[path] || routes["/"];
-	const module = await loadModule();
-	const { render } = module;
-	render(document.querySelector("#app"));
+    const path = window.location.pathname;
+    const loadModule = routes[path] || routes["/"];
+    const module = await loadModule();
+    const {render} = module;
+    render(document.querySelector("#app"));
 }
 
 document.addEventListener("click", (e) => {
-	const link = e.target.closest("a[data-link]");
-	if (link) {
-		e.preventDefault();
-		const url = link.getAttribute("href");
-		history.pushState(null, "", url);
-		handleRoute();
-	}
+    const link = e.target.closest("a[data-link]");
+    if (link) {
+        e.preventDefault();
+        const url = link.getAttribute("href");
+        history.pushState(null, "", url);
+        handleRoute();
+    }
 });
