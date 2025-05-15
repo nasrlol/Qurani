0dab3d6e7fe1b199648401e979c708c8472e7fd0
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-15 17:51:10 +0200

    added a buttonf or the folding mechanism and its styling

---
+++ src/components/foldButton.mjs
@@ -0,0 +1,33 @@
+"use strict";
+
+export const viewSurahFoldButton = `
+	<button class="fold" id="fold">View Surah</button>
+`
+
+export async function hideOnFold() {
+    console.log("function is running");
+
+    console.log("DOM content is loaded");
+
+    const button = document.getElementById("fold");
+    const quranOfTheDayView = document.getElementById("surahDay");
+    console.log(button, quranOfTheDayView);
+
+    quranOfTheDayView.style.height = "100%";
+
+    let expanded = false;
+
+    button.addEventListener("click", () => {
+        if (expanded) {
+            quranOfTheDayView.style.height = "0";
+            button.textContent = "View Surah";
+        } else {
+            quranOfTheDayView.style.height = "fit-content";
+
+            button.textContent = "Hide Surah";
+        }
+        expanded = !expanded;
+    });
+
+
+}
