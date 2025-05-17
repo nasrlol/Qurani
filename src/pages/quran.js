160ac07a08186de567add6199e3f30485646fbc7
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-17 23:31:22 +0200

    enabling the continuation after selecting a surah

--- src/pages/quran.js
+++ src/pages/quran.js
@@ -1,19 +1,55 @@
 "use strict";
 import {nav} from "../components/nav.mjs";
 import {footer} from "../components/footer.mjs";
-import {filterSearch, getSurahListOption} from "../api/surah.mjs";
-
+import {changeLanguage, filterSearch, getSurah, getSurahListOption} from "../api/surah.mjs";
 
 export async function render(container) {
-    container.innerHTML = `
-		${nav}
-	<main>
-		<input id="searchInput" class="searchInput">
-		<div class="selectSurah" id="selectSurah">
-			${await getSurahListOption()}
-		</div>
-	</main>
-	${footer}`;
 
-    await filterSearch()
+    const main = `
+         <main>
+            <div class="surahFields">
+                <input id="searchInput" class="searchInput" placeholder="Search for a Surah...">
+                <div>
+                <label for="language">Language</label>
+                <select name="language" id="selectLanguage">
+                    <option value="english">English</option>
+                    <option value="arabic">Arabic</option>
+                </select>   
+                </div>
+               
+            </div>
+           
+            <div class="selectSurah" id="selectSurah">
+                ${await getSurahListOption()}
+            </div>
+        </main>   
+    `
+    container.innerHTML = `
+        ${nav}
+        ${main}
+        ${footer}`;
+
+    document.getElementById("selectLanguage").addEventListener("change", async () => {
+        document.getElementById("selectSurah").innerHTML = await getSurahListOption(changeLanguage());
+        await filterSearch();
+    });
+
+    let options = document.getElementsByClassName("options");
+    for (let i = 0; i < options.length; i++) {
+
+        options[i].addEventListener("click", async () => {
+
+            container.innerHTML = `
+            ${nav}
+            <article class="arabic">
+                ${await getSurah(options[i].getAttribute("data-id"))}            
+            </article>
+            ${footer}
+            `
+
+        })
+    }
+
+
+    await filterSearch();
 }
