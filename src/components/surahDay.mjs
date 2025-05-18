6157e329f2a573ec4ba9ffd98a574e2b905490af
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-18 22:21:55 +0200

    18/05

--- src/components/surahDay.mjs
+++ src/components/surahDay.mjs
@@ -2,14 +2,14 @@
 
 import {getSurahRandom, surah} from "../api/surah.mjs";
 
-await getSurahRandom();
 
-export const surahOfTheDay = `
-<div class="surah" id="surahDay">
-    <p id="arabic" class="arabic">
-        ${surah.nameEnglish}
-        <br>
-        ${surah.surah} 
-    </p>
-</div
-`;
+await getSurahRandom();
+export const surahDay = `
+          <div class="surah" id="surahDay">
+            <p id="arabic" class="arabic">
+                ${surah.nameEnglish}
+                <br>
+                ${surah.surah} 
+            </p>
+        </div>    
+    `
