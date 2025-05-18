bea158516747f8e2796b3fb8ce8dd01584a84769
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-18 22:19:13 +0200

    added the search page

--- src/components/nav.mjs
+++ src/components/nav.mjs
@@ -7,6 +7,7 @@ export const nav = `
       <li><a href="/" data-link>Home</a></li> 
       <li><a href="/quran" data-link>Quran</a></li>
       <li><a href="/tafsir" data-link>Tafsir</a></li>
+      <li><a href="/search" data-link>Search</a></li>
       <li><a href="/about" data-link>About</a></li>
     </ul>
   </nav>
