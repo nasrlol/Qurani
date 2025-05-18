a3f08730e4fc7caa92ddd62fb6ecb2e1d8c225be
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-18 22:18:51 +0200

    changd id names, and made one async because i thought that was the bug instead of the opacity thing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,10 @@
 "use strict";
 
 import {nav} from "../components/nav.mjs";
-import {ayahOfTheDay} from "../components/ayahDay.mjs";
+import {getAyahOfTheDay} from "../components/ayahDay.mjs";
 import {footer} from "../components/footer.mjs";
-import {surahOfTheDay} from "../components/surahDay.mjs";
 import {hideOnFold, viewSurahFoldButton} from "../components/foldButton.mjs";
+import {surahDay} from "../components/surahDay.mjs";
 
 
 export async function render(container) {
@@ -19,13 +19,13 @@ export async function render(container) {
 		<div>
 			<section id="ayah-of-the-day">
 				<h2>Ayah of the Day</h2>
-				${ayahOfTheDay}
+				${await getAyahOfTheDay()}
 			</section>
 			<section id="surah-of-the-day">
 				<h2>Surah of the day </h2>
 				<br>
 				${viewSurahFoldButton}
-				${surahOfTheDay}
+				${surahDay}
 			</section>
 		</div>
 	</main>
