835e102cf3f0dad37c4f465f4afa4f1c2c2c1ff8
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-18 22:17:37 +0200

    was having a bug but that was caused by 0 opacity on #app and i thought this could be the problem so i changed the variable into a function to make sure the functions getAyah() and getAyahAudio() get executed before the variable was loaded

--- src/components/ayahDay.mjs
+++ src/components/ayahDay.mjs
@@ -2,19 +2,22 @@
 
 import {ayah, getAyah, getAyahAudio} from "../api/ayah.mjs";
 
-await getAyah()
-await getAyahAudio(ayah.numberQuran);
 
-export const ayahOfTheDay = `
-<div class="ayahOfTheDay">
-    <p id="arabic" class="arabic">
-        ${ayah.surahNameEnglish}
-        <br>
-        ${ayah.ayahNumber} ${ayah.ayah} 
-        <br>
-          <audio controls>
-            <source src=${ayah.audioURL} type="audio/mpeg">
-        </audio>
-    </p>
-</div>
-`;
+export async function getAyahOfTheDay() {
+    await getAyah()
+    await getAyahAudio(ayah.numberQuran);
+
+    return `
+        <div class="ayahOfTheDay">
+            <p id="arabic" class="arabic">
+                ${ayah.surahNameEnglish}
+                <br>
+                ${ayah.ayahNumber} ${ayah.ayah} 
+                <br>
+                  <audio controls>
+                    <source src=${ayah.audioURL} type="audio/mpeg">
+                </audio>
+            </p>
+        </div>
+`
+}
