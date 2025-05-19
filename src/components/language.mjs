2c8b58903c822b44b050ea0ec64ff274a1599b4d
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-19 22:31:24 +0200

    containerization

--- src/components/language.mjs
+++ src/components/language.mjs
@@ -3,11 +3,14 @@
 export function languageDropdown(id) {
 
     return `
+<div>
        <label>Select a language</label>
-       <select id=${id.toString()}>
+       <select id=${id}>
             <option value="en">English</option> 
             <option value="ar">Arabic</option> 
             <option value="fr">French</optionv> 
        </select> 
+</div>
+
     `
 }
