db3d16eeb0508ff35ab8ce091d2e39c3b0edb4a5
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-19 22:36:36 +0200

    fixed the search function implementation and added and made it asynchronous

--- src/pages/search.mjs
+++ src/pages/search.mjs
@@ -2,30 +2,36 @@
 
 import {nav} from "../components/nav.mjs";
 import {footer} from "../components/footer.mjs";
+import {searchKeyword} from "../api/surah.mjs";
+import {languageDropdown} from "../components/language.mjs";
 
 
-export function render(container) {
+export async function render(container) {
+
 
     container.innerHTML = `
-
         ${nav} 
         <main>
-        
-            <input id="search" placeholder="search"></input> 
-            <button id="searchButton" type="submit"></button>
-        
+            <div class="searchContainer">
+                <input id="search" placeholder="search">
+                <button id="searchButton" >Search</button>
+                <div id="searchResult"></div>
+                ${languageDropdown()}
+            </div>
         </main>
         ${footer}
     `
 
     const submitSearchButton = document.getElementById("searchButton");
     const searchData = document.getElementById("search");
-    searchData.addEventListener("keypress", () => {
-        if (event.key !== "Enter") {
-            console.log("enter received");
+    const searchResult = document.getElementById("searchResult");
+
+
+    submitSearchButton.addEventListener("click", async () => {
+
+        const input = searchData.value;
+        if (input !== "") {
+            searchResult.innerHTML = await searchKeyword(input);
         }
-    })
-    submitSearchButton.addEventListener("submit", () => {
-        console.log("submit received");
     })
 }
