ea8f4efb86f1de23798b11370a807578130e599b
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-21 23:44:07 +0200

    removed debigging console.log and white lines

--- src/components/collapse-buttons.mjs
+++ src/components/collapse-buttons.mjs
@@ -4,25 +4,19 @@ export const viewSurahFoldButton = `
 	<button class="fold" id="fold">View Surah</button>
 `
 
-export async function hideOnFold() {
-    console.log("function is running");
-
-    console.log("DOM content is loaded");
+export async function fold() {
 
     const button = document.getElementById("fold");
     const quranOfTheDayView = document.getElementById("surahDay");
     console.log(button, quranOfTheDayView);
 
-
     let expanded = false;
-
     button.addEventListener("click", () => {
         if (expanded) {
             quranOfTheDayView.style.height = "0";
             button.textContent = "View Surah";
         } else {
             quranOfTheDayView.style.height = "fit-content";
-
             button.textContent = "Hide Surah";
         }
         expanded = !expanded;
