f49f1c9f25c5a563e9efdae85c7c30bbb0965272
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-21 23:47:39 +0200

    retrieving the translation aeditions from the api for specifiying the search also moved the search function

---
+++ src/api/keywords.mjs
@@ -0,0 +1,49 @@
+"use strict";
+
+// the quran cloud api provides an endpoint that returns all the available editions of translations etc.
+// this will allow me to provide a better language dropdown module for retrieving translations
+
+export async function getEditions() {
+
+    const url = "http://api.alquran.cloud/v1/edition"
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("failed to fetch the URL");
+
+        const data = await response.json();
+        console.log(data);
+        return data.data.map((edition) => {
+            const option = document.createElement("option");
+            option.value = edition.identifier;
+            option.textContent = edition.language;
+            return option;
+        })
+    } catch (error) {
+        console.error("failed to retrieve the editions: ", error);
+        return null;
+    }
+}
+
+export async function searchKeyword(string) {
+
+    const url = "http://api.alquran.cloud/v1/search"
+    const languageValue = document.getElementById("languageValues").value;
+    const location = document.getElementById("surahDropdownList").value;
+    let newURL;
+    newURL = `${url}/${string}/${location}/${languageValue}`;
+    console.log(newURL);
+
+    try {
+        const response = await fetch(newURL);
+        if (!response.ok) throw new Error("There was trouble finding the ayah you searched for");
+
+        const data = await response.json();
+        console.log("data: ", data);
+
+        console.log(data.data.count);
+        return data.data.matches.map((ayah) => `<p>${ayah.number}:${ayah.surah.number} ${ayah.text}</p><br>`).join("");
+
+    } catch (error) {
+        console.error("No keywords were found");
+    }
+}
