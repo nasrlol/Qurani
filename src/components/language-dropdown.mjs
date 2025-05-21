b0e02e980e5a404336fb5230e124c9865a8824ca
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-21 23:48:22 +0200

    adding the optoins from the retrieved editions and renamed the file

---
+++ src/components/language-dropdown.mjs
@@ -0,0 +1,10 @@
+"use strict";
+
+export async function languageDropdown() {
+
+    return `
+           <label>Select a language</label>
+           <select id=languageValues>
+           </select> 
+    `
+}
