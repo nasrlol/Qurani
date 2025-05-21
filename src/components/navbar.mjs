2d645999af4bad8b17a9f76c1ad38743eec60f75
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-21 23:48:41 +0200

    added a new page and the theme button

--- src/components/navbar.mjs
+++ src/components/navbar.mjs
@@ -1,14 +1,16 @@
 "use strict";
 
-export const nav = `
+export const navbar = `
 <header>
   <nav>
     <ul>
       <li><a href="/" data-link>Home</a></li> 
       <li><a href="/quran" data-link>Quran</a></li>
-      <li><a href="/tafsir" data-link>Tafsir</a></li>
+      <li><a href="/translation" data-link>Translation</a></li>
       <li><a href="/search" data-link>Search</a></li>
+      <li><a href="/favorites" data-link>Favorites</a></lihref>
       <li><a href="/about" data-link>About</a></li>
+      <button class="themeButton" id="theme">&#x2600</button>
     </ul>
   </nav>
 </header>
