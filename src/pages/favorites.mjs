a624a721d0e769d30ff70aaf9f27e8999868627a
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-22 17:39:21 +0200

    adding the selected favorite surahs to the output, retrieving them from the click event, then checking if they are in the local cache and updating the list, and then outputting that list

--- src/pages/favorites.mjs
+++ src/pages/favorites.mjs
@@ -3,21 +3,37 @@
 import {navbar} from "../components/navbar.mjs";
 import {footer} from "../components/footer.mjs";
 import {favoritesList} from "../components/favorites-list.mjs";
+import {changeTheme} from "../utils/theme-utils.mjs";
 
 export function render(container) {
-    let output = "";
-
-    favoritesList.forEach((surah) => {
-        output += `<button class="options">${surah.surahName}</button>`;
-    });
 
     container.innerHTML = `
         ${navbar}
         <main>
-        <h2>Favorite Surahs</h2>
-            ${output}
+            <h2>Favorite Surahs</h2>
+            <div class="selectSurah" id="favoritesList">
+            
+            </div>
         </main>
         ${footer}
     `;
 
+    const output = document.getElementById("favoritesList");
+
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    favoritesList.splice(0, favoritesList.length, ...storedFavorites);
+
+    if (favoritesList.length === 0) {
+        output.append("");
+        const notFound = document.createElement("p");
+        notFound.textContent = "No favorite Surahs saved yet."
+    } else {
+        favoritesList.forEach((surah) => {
+            const button = document.createElement("button");
+            button.textContent = surah;
+            output.append(button);
+        });
+    }
+
+    changeTheme();
 }
