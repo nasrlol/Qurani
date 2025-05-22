a3424eae9c1ff1593b52b607e1001b9218add619
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-22 17:51:07 +0200

    planning on using this to expand and hide the navigation bar on the mobile version, don't know if it will get finished on time though

--- src/components/navbar.mjs
+++ src/components/navbar.mjs
@@ -8,10 +8,17 @@ export const navbar = `
       <li><a href="/quran" data-link>Quran</a></li>
       <li><a href="/translation" data-link>Translation</a></li>
       <li><a href="/search" data-link>Search</a></li>
-      <li><a href="/favorites" data-link>Favorites</a></lihref>
+      <li><a href="/favorites" data-link>Favorites</a></li>
       <li><a href="/about" data-link>About</a></li>
       <button class="themeButton" id="theme">&#x2600</button>
+      
+        <div class="expand" id="expand">
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+        </div>
     </ul>
+
   </nav>
 </header>
 `;
