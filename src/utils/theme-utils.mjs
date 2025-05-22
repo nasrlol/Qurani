3ecc65e0cee4b2e44c1134a5275dac7e8806cecc
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-22 22:50:10 +0200

    removed a couple of white lines

--- src/utils/theme-utils.mjs
+++ src/utils/theme-utils.mjs
@@ -3,26 +3,18 @@
 export function changeTheme() {
 
     const button = document.getElementById("theme");
-
-
     let darkMode = localStorage.getItem("darkMode") === "true";
 
     if (darkMode === true) {
-
         document.documentElement.classList.add("dark-theme");
         document.body.classList.add("dark-theme");
-
         button.classList.add("dark-theme");
-
     }
 
     button.addEventListener("click", () => {
-
         document.documentElement.classList.toggle("dark-theme");
         document.body.classList.toggle("dark-theme");
-
         button.classList.toggle("dark-theme");
-
         darkMode = !darkMode;
         localStorage.setItem("darkMode", darkMode);
     });
