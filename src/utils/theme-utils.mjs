d34f184635ae86ed93514807157217c3fc976055
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-22 17:40:02 +0200

    realised the setting the arabic class to dark-theme wasnt having an effect so removed it

--- src/utils/theme-utils.mjs
+++ src/utils/theme-utils.mjs
@@ -3,7 +3,6 @@
 export function changeTheme() {
 
     const button = document.getElementById("theme");
-    const arabic = document.getElementById("arabic");
 
 
     let darkMode = localStorage.getItem("darkMode") === "true";
@@ -14,7 +13,6 @@ export function changeTheme() {
         document.body.classList.add("dark-theme");
 
         button.classList.add("dark-theme");
-        arabic.classList.add("dark-theme");
 
     }
 
@@ -24,7 +22,6 @@ export function changeTheme() {
         document.body.classList.toggle("dark-theme");
 
         button.classList.toggle("dark-theme");
-        arabic.classList.toggle("dark-theme");
 
         darkMode = !darkMode;
         localStorage.setItem("darkMode", darkMode);
