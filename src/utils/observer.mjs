51ffd81005f4c6931b08c43d5b5f26c7c3be433c
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-22 23:28:56 +0200

    van 3 naar 6 kolommen

--- src/utils/observer.mjs
+++ src/utils/observer.mjs
@@ -10,9 +10,9 @@ export function setObserver() {
             if (width < 600) {
                 surahOptions.style.gridTemplateColumns = "repeat(1, 1fr)";
             } else if (width < 900) {
-                surahOptions.style.gridTemplateColumns = "repeat(2, 1fr)";
-            } else {
                 surahOptions.style.gridTemplateColumns = "repeat(3, 1fr)";
+            } else {
+                surahOptions.style.gridTemplateColumns = "repeat(6, 1fr)";
             }
         }
     });
