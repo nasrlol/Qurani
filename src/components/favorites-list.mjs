5c4db3e2588c956377d14017616030caffd2f745
Abdellah El Morabit <nsrddyn@gmail.com>
2025-05-22 22:48:59 +0200

    replaced the event listener to an onclick function because i ended up keeping on adding event listeners to the button

--- src/components/favorites-list.mjs
+++ src/components/favorites-list.mjs
@@ -1,44 +1,31 @@
 "use strict";
 
 export let favoritesList = [];
-
-class Surah {
-    constructor(surahName) {
-        this.surahName = surahName;
-    }
-}
+export const favoritesButton = `<button id="setFavorite" class="setFavorite">☆</button>`
 
 export function favorite(surahName) {
-    let favorite = false;
     const favoriteButton = document.getElementById("setFavorite");
-
-    let surah = new Surah(surahName);
-
-    favoriteButton.addEventListener("click", () => {
-        if (!favorite) {
+    const updateButton = () => {
+        if (favoritesList.includes(surahName)) {
             favoriteButton.textContent = "★";
-            favoriteButton.style.color = "yellow";
-            favoritesList.push(surah);
-
-            // way of debugging if  the surah name was added to the list
-            console.log(favoritesList);
-
         } else {
-
             favoriteButton.textContent = "☆";
-            favoriteButton.style.color = "white";
-
-            favoritesList.pop();
-
-            // way of debugging if  the surah name was added to the list
-            console.log(favoritesList);
-
-            favorite = !favorite;
         }
-    })
+    }
+
+    favoriteButton.onclick = () => {
+
+        if (!favoritesList.includes(surahName)) {
+            favoritesList.push(surahName);
+            favoriteButton.textContent = "★";
+        } else {
+            const index = favoritesList.indexOf(surahName);
+            favoritesList.splice(index, 1);
+        }
+
+        localStorage.setItem("favorites", JSON.stringify(favoritesList));
+        updateButton();
+    }
+
 }
 
-
-export const favoritesButton = `
-    <button id="setFavorite" class="setFavorite">☆</button>
-`
